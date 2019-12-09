e224c20bc1d2bc57e6a116ea403bacc9b5d1254a
takuya kikuchi <t.kikuchi.pch@gmail.com>
2019-12-10 00:24:22 +0900

    Update script

--- dist/index.js
+++ dist/index.js
@@ -6307,35 +6307,53 @@ core.debug("Starting...");
 
 async function main() {
 
-  const sourceBranchName = core.getInput('SOURCE_BRANCH_NAME');
   const owner = core.getInput('REPOSITORY_OWNER_NAME');
   const repo = core.getInput('REPOSITORY_NAME');
   const apiKey = core.getInput('GITHUB_TOKEN');
   const mergedPRNumber = core.getInput('MERGED_PR_NUMBER');
-  const mergedPRTitle = core.getInput('MERGED_PR_TITLE');
 
-  core.debug(`sourceBranchName: ${sourceBranchName}`);
   core.debug(`owner: ${owner}`);
   core.debug(`repo: ${repo}`);
   core.debug(`mergedPRNumber: ${mergedPRNumber}`);
-  core.debug(`mergedPRTitle: ${mergedPRTitle}`);
 
   const octokit = new Octokit({ auth: apiKey });
+
+  // Get information of a merged PR
+  const mergedPR = await octokit.pulls.get({
+    owner: owner,
+    repo: repo,
+    pull_number: mergedPRNumber
+  });
+
+  if(mergedPR == null) {
+    core.debug(`A PR #${mergedPRNumber} does not exist`);
+    return
+  }
+  if(!mergedPR.data.merged) {
+    core.debug(`A PR #${mergedPRNumber} is not merged. Skipping.`);
+  }
+
+  core.debug(`merged PR: ${mergedPR.data.base.label}`);
+  const sourceBranchLabel = mergedPR.data.base.label
+  const mergedPRTitle = mergedPR.data.title
+
+  // Get an existing PR of a base branch of the merged PR
   const pulls = await octokit.pulls.list({
     owner: owner,
     repo: repo,
-    head: `${owner}:${sourceBranchName}`
+    head: sourceBranchLabel
   });
   const pr = pulls.data.shift();
 
   // Do nothing when got an invalid PR
-  if (pr.head.ref != sourceBranchName) {
+  if (pr.head.label != sourceBranchLabel) {
     return
   }
 
   core.debug(`Got pull request: ${pr.number}`)
   const currentBody = pr.body;
 
+  // Update PR description of the branch
   await octokit.pulls.update({
     owner: owner,
     repo: repo,
